a22bcbad2a8de094ffe9a5f2ba20b8a9f8af5f44
Hiroki Yagita <yagihiro@gmail.com>
2010-03-06 12:31:52 +0900

    Fix spec to fail.

--- spec/fs-crawler_spec.rb
+++ spec/fs-crawler_spec.rb
@@ -1,7 +1,19 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "FsCrawler" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+describe "Crawler::Filesystem" do
+
+  before(:all) do
+    @options = {
+      :port             => 30001,
+      :host             => "0.0.0.0",
+      :distination_port => 30002,
+      :distination_host => "0.0.0.0",
+      :config           => "./.fs-crawler.yml"
+    }
   end
+  
+  it "construction" do
+    Crawler::Filesystem.new(@options).should_not be_nil
+  end
+  
 end
